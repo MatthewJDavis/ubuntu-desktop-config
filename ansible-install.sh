93591c537dbbe572d15402c3e129ba06444534ae
MatthewJDavis <matthewdavis111@gmail.com>
2018-09-16 19:08:24 -0400

    added ansible config bash script

---
+++ ansible-install.sh
@@ -0,0 +1,8 @@
+!# /bin/bash
+# Install ansible on Ubuntu
+
+apt-get update -y
+apt-get install software-properties-common -y
+apt-add-repository ppa:ansible/ansible -y
+apt-get update -y
+apt-get install ansible -y
